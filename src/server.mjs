5bc61a182db7c46a2f15251a0aaffefd20841137
Alex Frantz <alex@alexavfrantz.com>
2024-04-24 22:32:57 -0400

    add root redirect

--- src/server.mjs
+++ src/server.mjs
@@ -12,6 +12,10 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req,res) => {
+    return res.redirect("https://alexav.gg")
+})
+
 app.use('/upload', UploadRoute);
 app.use('/info', InfoRoute);
 
